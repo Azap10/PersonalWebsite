f4251fc74ffc0548a39249c29217c6195d22227f
Adam Zapatka <adamzapatka@gmail.com>
2025-03-20 22:52:17 +0800

    pivoted to musical tracker site to help friends

---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+console.log('JavaScript file is linked correctly.');
+alert('JavaScript file is linked correctly.');
+
+function projectsColor() {
+    var elements = document.querySelectorAll('.test');
+    
+    elements.forEach(function(element) {
+        element.style.color = 'red'; // Change the color to red
+    });
+}
+
+function resumeColor() {
+    var elements = document.querySelectorAll('.test');
+    
+    elements.forEach(function(element) {
+        element.style.color = 'red'; // Change the color to red
+    });
+}
+
+function linkedinColor() {
+    var elements = document.querySelectorAll('.test');
+    
+    elements.forEach(function(element) {
+        element.style.color = 'red'; // Change the color to red
+    });
+}
+
+function blogColor() {
+    var elements = document.querySelectorAll('.test');
+    
+    elements.forEach(function(element) {
+        element.style.color = 'red'; // Change the color to red
+    });
+}
